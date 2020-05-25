787cdb1ddcb4185fdb29755ba38b1104a16f9fbe
daovanduc <duc07092000@gmail.com>
2020-05-25 10:18:20 +0700

    create form email input

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="main-content">
+        <h1>Ứng dụng kiểm tra email hợp lệ</h1>
+        <form method="post">
+            <label for="email-input">Email:</label>
+            <input type="text" id="email-input" name="email" placeholder="Nhập email cần kiểm tra">
+            <input type="submit" value="Check">
+        </form>
+    </div>
+</div>
